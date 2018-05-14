aefc608776878d570e962274e59fba1c7e1e6ecc
itReverie <brenda@itreverie.com>
2018-05-14 11:23:11 -0400

    Adding actions and reducer

---
+++ src/store/reducers/places.js
@@ -0,0 +1,13 @@
+const initialState = {
+    places:[], 
+    selectedPlace: null,
+};
+
+const reducer=(oldState=initialState, action)=>{
+    switch(action.type){
+        default:
+                return state;
+    }
+};
+
+export default reducer;
